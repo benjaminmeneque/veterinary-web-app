844813d4f2c95fe88893b12c82b43bc77f362dc2
Benjamin <benjamin.cornejo.meneque@gmail.com>
2024-03-18 17:24:16 +0800

    make admin more informative and update requirements.txt

--- booking/admin.py
+++ booking/admin.py
@@ -3,25 +3,41 @@ from django.contrib import admin
 from booking import models
 
 
-# Register your models here.
 class SpecialtiesAdmin(admin.ModelAdmin):
     pass
 
 
 class DoctorAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["first_name", "last_name", "email", "phone_number"]
+    empty_value_display = "-empty-"
+    search_fields = ["specialties__name"]
 
 
 class VeterinaryAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["name", "address", "contact_number"]
+    search_fields = ["name"]
+    ordering = ["name"]
 
 
 class PetAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["name", "date_birth", "breed"]
+    search_fields = ["name", "breed"]
+    ordering = ["name"]
 
 
 class VeterinaryInstanceAdmin(admin.ModelAdmin):
-    pass
+    list_display = [
+        "id",
+        "veterinary_name",
+        "doctor_name",
+        "owner",
+        "pet_name",
+        "create_date",
+        "status",
+    ]
+    date_hierarchy = "create_date"
+    empty_value_display = "-empty-"
+    ordering = ["create_date"]
 
 
 admin.site.register(models.Specialties, SpecialtiesAdmin)
